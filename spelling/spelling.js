3bc35b297eac8fdaee88019d39547566c1f7c8ef
Daniel Russ <druss@mail.nih.gov>
2023-03-06 17:54:31 -0500

    finally!

--- spelling/spelling.js
+++ spelling/spelling.js
@@ -1,57 +1,46 @@
 import localforage from 'https://cdn.skypack.dev/localforage';
 
-
 let hunspell_dictionary_url = "https://raw.githubusercontent.com/elastic/hunspell/master/dicts/en_US/en_US.dic"
 let soccer_dictionary_url = "./soccer_en_US.dic"
 
 export async function loadDictionary(dictionary_url) {
-
-    //let dictionary_url = "https://cgit.freedesktop.org/libreoffice/dictionaries/tree/en"
     let utf8decoder = new TextDecoder("Windows-1252");
     let dict = new Set()
     const regex = new RegExp('^[a-z]');
     function addLine(line) {
         if (regex.test(line)) {
             dict.add(line.split("/", 1)[0])
-            //console.log("adding line: ", line)
         }
     }
 
+    let oldText = ""
+    let lineCount = 0;
+    let blockCount = 0;
+    let stream = (await fetch(dictionary_url)).body
+    const reader = stream.getReader();
 
-    await fetch(dictionary_url)
-        .then(response => {
-            let reader = response.body.getReader()
-            let oldText = ""
-            reader.read().then(
-                function processText({ done, value }) {
-                    console.log("... reading new block ... ",dictionary_url)
+    for (let block = await reader.read(); !block.done; block = await reader.read()) {
+        blockCount++;
 
-                    if (done) {
-                        console.log("Stream complete ... ", value);
-                        return done;
-                    }
+        let txt = oldText + utf8decoder.decode(block.value)
+        let lines = txt.split("\n");
+        oldText = lines.pop();
+        lines.forEach((line) => addLine(line))
 
-
-                    let txt = utf8decoder.decode(value)
-                    let lines = txt.split("\n");
-
-                    // add the unfinished line from the last chunk
-                    // to the 1st line of the current chunk...
-                    lines[0] = oldText + lines[0]
-                    oldText = lines.pop();
-                    lines.forEach((line) => addLine(line))
-
-                    return reader.read().then(processText);
-                })
-        })
-    return dict;
+        lineCount += lines.length
+    }
+    return dict
 }
 
 async function create_dictionary() {
     let p1 = loadDictionary(hunspell_dictionary_url)
-    let p2 = loadDictionary(soccer_dictionary_url)
-    return await Promise.all([p1,p2]).then( ([s1,s2]) => {console.log(...s2); return new Set([...s1,...s2])} )
+    let p2 = loadDictionary(soccer_dictionary_url);
+    return await Promise.all([p1,p2]).then( ([s1,s2])=> new Set([...s1,...s2]) );
 }
+
 
 console.log("in spelling ", localforage)
 window.dict = await create_dictionary()
+window.create_dictionary = create_dictionary
+console.log(dict.has("endodontist"))
+console.log(dict.has('zygote'))
